6e9d529eb4e23dbc802e8c18d795b12c92cc65de
Rajiv <ray_rajiv@rediffmail.com>
2019-03-31 13:59:25 -0400

    Adds views to create/edit blog post inside the site.

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+# Forms for our models
+
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text', )
